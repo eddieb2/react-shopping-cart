6d418175e7d73f3cc2ff2d519e414c2ed3237aed
Edward Blanciak <blanciak.edward2@gmail.com>
2020-03-19 16:03:00 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+import { ProductContext } from "./context/ProductContext";
+import { CartContext } from "./context/CartContext";
 
-// Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
-
+//* COMPONENT HERE *//
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState(data);
+  console.log("cart in app.js:", cart);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = item => {
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  const removeItem = id => {};
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+  return (
+    // ? QUESTION: DOES IT MATTER IF I HAD MY VALUES IN AN ARRAY OR OBJ? Is this just preference?
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CartContext.Provider value={{ cart }}>
+        <div className="App">
+          <Navigation />
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
+
+          <Route path="/cart">
+            <ShoppingCart />
+          </Route>
+        </div>
+      </CartContext.Provider>
+    </ProductContext.Provider>
+  );
 }
-
 export default App;
